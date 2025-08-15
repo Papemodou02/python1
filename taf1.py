788a1eec4da092d56789289f7861114295b2b966
Papemodou02 <papemodoutabarafall00@gmail.com>
2025-08-15 16:19:08 +0000

    modification du taf1.py en une différence

--- taf1.py
+++ taf1.py
@@ -1,6 +1,6 @@
 a=int(input("valeur de a:"))
 b=int(input("valeur de b:"))
-somme= a+b
-print("la somme de a et b est =", somme)
+différence= a-b
+print("la différence de a et b est =", différence)
 print("modification du taf1")
-
+print("programme amélioré pour calculer une différence")
